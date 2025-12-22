9ce660eff8b534d9b42c1b09ac97a59196ae3b76
Dmytro-Doronin <doronindmytro623@gmail.com>
2025-12-22 21:29:30 +0100

    feat: next js start and styles

---
+++ client/src/app/(public)/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+
+import styles from './layout.module.scss';
+
+export default function PublicLayout({ children }: { children: ReactNode }) {
+    return <div className={styles.authLayout}>{children}</div>;
+}
---
+++ client/src/app/(public)/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function HomePage() {
+    return (
+        <main className={styles.container}>
+            <div>Hello</div>
+        </main>
+    );
+}
